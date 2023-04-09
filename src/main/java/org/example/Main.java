43c60a56df3e505f7e514af20c2160810bce6aed
an-savrasov <130032294+an-savrasov@users.noreply.github.com>
2023-04-09 09:53:43 +0300

    bot is created

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,30 @@
 package org.example;
 
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Create your bot passing the token received from @BotFather
+        TelegramBot bot = new TelegramBot("5555375819:AAH9XxsvYzAQX29vFMJKX_u4RnK65oQL3mA");
+
+// Register for updates
+        bot.setUpdatesListener(updates -> {
+            // ... process updates
+            // return id of last processed update or confirm them all
+            for (int i = 0; i < updates.size(); i++) {
+                Update update = updates.get(i);
+                System.out.println(update.message().text());
+                long chatId = update.message().chat().id();
+                SendResponse response = bot.execute(new SendMessage(chatId, update.message().text()));
+            }
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+        });
+
+
+
     }
 }
