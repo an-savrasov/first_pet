9330bdb3f83fc3cf8b0e577fa827cb8141a86d67
an-savrasov <130032294+an-savrasov@users.noreply.github.com>
2023-04-26 22:20:28 +0300

    token in config.properties

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,27 +5,14 @@ import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
-import org.jetbrains.annotations.NotNull;
 
-import java.io.*;
-import java.util.Properties;
 public class Main {
-
+    static Token token = new Token();
+    public static final String token1 = token.getToken();
     public static void main (String[] args) {
 
-        FileInputStream fis;
-        Properties property = new Properties();
 
-        try {
-            fis = new FileInputStream("src/main/resources/config.properties");
-            property.load(fis);
-            String token = property.getProperty("token");
-
-        } catch (IOException e) {
-            System.err.println("Error: Файл свойств отсуствует!");
-        }
-
-        TelegramBot bot = new TelegramBot("token");
+        TelegramBot bot = new TelegramBot(token1);
 
         bot.setUpdatesListener(updates -> {
             for (int i = 0; i < updates.size(); i++) {
@@ -36,21 +23,9 @@ public class Main {
             }
             return UpdatesListener.CONFIRMED_UPDATES_ALL;
 
-    /*public static void main(String[] args) {
-        TelegramBot bot = new TelegramBot("token");
 
-
-        bot.setUpdatesListener(updates -> {
-            for (int i = 0; i < updates.size(); i++) {
-                Update update = updates.get(i);
-                System.out.println(update.message().text());
-                long chatId = update.message().chat().id();
-                SendResponse response = bot.execute(new SendMessage(chatId, update.message().text()));
-            }
-            return UpdatesListener.CONFIRMED_UPDATES_ALL;
-        });
-
-    }*/
 
         });
-    }}
+    }
+
+}
---
+++ src/main/java/org/example/Token.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Token {
+    private String token;
+
+    public Token() {
+        FileInputStream fis;
+        Properties property = new Properties();
+        try {
+            fis = new FileInputStream("src/main/resources/config.properties");
+            property.load(fis);
+            token = property.getProperty("token");
+
+        } catch (IOException e) {
+            System.err.println("Error: Файл свойств отсуствует!");
+        }
+    }
+
+    public String getToken() {return token;}
+}
